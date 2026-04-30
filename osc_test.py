72b9708f446b17719592453d2b04d79149b01289
Shadowtrot <caucow98@gmail.com>
2026-04-30 02:33:12 -0500

    Implemented radial OSC control test (F/B + L/R)

--- osc_test.py
+++ osc_test.py
@@ -2,6 +2,7 @@ from Raspbot_Lib import Raspbot
 import socket
 import struct
 import math
+import time
 
 
 class BotRef:
@@ -15,20 +16,28 @@ class BotRef:
         self.bot.Ctrl_Muto(3, 0)
         self.bot.Ctrl_Servo(1, 90)
         self.bot.Ctrl_Servo(2, 25)
-        print("Exiting!!!")
+        print(">>> Exiting and stopping bot.")
 
 
 bot_ref = BotRef(Raspbot())
 
 ROOT2 = math.sqrt(2)
+# How far out of phase is wheel rotation from motion vector
 WHEEL_PHASE = math.pi / 4
-MAX_SPEED = 100
+# Within [0, 255]
+# WARNING: Too high a value will draw too much power, RaspberryPi
+#        : CPU voltage will fall below tolerance and reset;
+#        : 100 is a safe value.
+MAX_SPEED = 99
 
 
 def ctrl_grid(x, y):
+    # Radians
     move_angle = math.atan2(y, x)
+    # Within [-1.0, 1.0]
     wheel_cos = math.cos(move_angle - WHEEL_PHASE)
     wheel_sin = math.sin(move_angle - WHEEL_PHASE)
+    # Within [0.0, 1.0]
     speed = math.sqrt(x * x + y * y) / ROOT2
 
     fl = wheel_cos * speed * MAX_SPEED
@@ -44,12 +53,48 @@ def ctrl_grid(x, y):
     bot_ref.bot.Ctrl_Muto(1, round(rl))
     bot_ref.bot.Ctrl_Muto(2, round(fr))
     bot_ref.bot.Ctrl_Muto(3, round(rr))
+    time.sleep(0.001)
 
 
 def ctrl_radial(r, a):
-    a -= 0.5
+    # Up/Down is full forwards/backwards
+    #   a = 0.0 -> Up
+    #   a = 0.5 -> Down
+    #   a = 1.0 -> Up
+    # Left/Right is full rotate
+    #   a = 0.25 -> Right
+    #   a = 0.75 -> Left
 
-    print(r, a)
+    # [-1.0, 1.0] is [backwards, forwards]
+    drive_weight = 0
+    # [-1.0, 1.0] is [left, right]
+    turn_weight = 0
+
+    if a < 0.5:
+        turn_weight = 1 - abs(4 * (a - 0.25))
+    else:
+        turn_weight = 1 - abs(4 * (a - 0.75))
+    drive_weight = 1 - turn_weight
+
+    if 0.25 < a < 0.75:
+        drive_weight = -drive_weight
+    if a > 0.5:
+        turn_weight = -turn_weight
+
+    fl = (drive_weight + turn_weight) * r * MAX_SPEED
+    rl = (drive_weight + turn_weight) * r * MAX_SPEED
+    fr = (drive_weight - turn_weight) * r * MAX_SPEED
+    rr = (drive_weight - turn_weight) * r * MAX_SPEED
+    print(format(fl, '.3'), format(fr, '.3'))
+    print(format(rl, '.3'), format(rr, '.3'))
+    print(format(r * MAX_SPEED, '.3'))
+    print()
+
+    bot_ref.bot.Ctrl_Muto(0, round(fl))
+    bot_ref.bot.Ctrl_Muto(1, round(rl))
+    bot_ref.bot.Ctrl_Muto(2, round(fr))
+    bot_ref.bot.Ctrl_Muto(3, round(rr))
+    time.sleep(0.001)
 
 
 def read_int(buf, i):
