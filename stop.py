6a5f63e016c387b19c221547ac2482886ef06164
Shadowtrot <caucow98@gmail.com>
2026-04-30 00:11:33 -0500

    Added emergency stop script to home all the motor controls

---
+++ stop.py
@@ -0,0 +1,10 @@
+from Raspbot_Lib import Raspbot
+
+
+bot = Raspbot()
+bot.Ctrl_Muto(0, 0)
+bot.Ctrl_Muto(1, 0)
+bot.Ctrl_Muto(2, 0)
+bot.Ctrl_Muto(3, 0)
+bot.Ctrl_Servo(1, 90)
+bot.Ctrl_Servo(2, 25)
