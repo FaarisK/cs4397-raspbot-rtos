892bb24edeae16ba6b25c350e7e8cfba87efd786
Shadowtrot <caucow98@gmail.com>
2026-05-02 06:17:23 -0500

    Added notes regarding camera and image processing.

---
+++ vision.py
@@ -0,0 +1,7 @@
+# NOTE: Camera can safely capture at 15 FPS (0.066~ seconds/frame) with sub-
+#       millisecond latency per frame. Capturing at higher framerates (e.g. 20
+#       FPS) *WILL* result in blocking while the camera buffers the next frame.
+
+# NOTE: Just looping over all pixels can take 100-200ms, an optimized image scan
+#       is needed. Multi-threaded/multi-process image processing may not be a
+#       terrible idea (barring deadline constraints).
