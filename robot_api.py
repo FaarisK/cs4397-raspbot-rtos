9678fd604e588630294ed8c8f0916ea158dd2a53
Shadowtrot <caucow98@gmail.com>
2026-05-05 14:37:26 -0500

    Implemented state interpolation on update (robot_api)

--- robot_api.py
+++ robot_api.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 import time
 from copy import copy
 
@@ -6,15 +7,21 @@ from Raspbot_Lib import Raspbot
 
 
 class CameraAngle:
-    def __init__(self, yaw, pitch):
+    def __init__(self, yaw: int, pitch: int):
         self.yaw = yaw
         self.pitch = pitch
 
+    def __str__(self):
+        return f'({self.yaw}, {self.pitch})'
+
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return f'({format(self.x, '.3')}, {format(self.y, '.3')})'
 
 
 class MotorState:
@@ -23,13 +30,14 @@ class MotorState:
         self.rl = 0
         self.fr = 0
         self.rr = 0
-        self.vx = 0
-        self.vy = 0
-        self.va = 0
+        self.vx = 0.0
+        self.vy = 0.0
+        self.va = 0.0
 
 
 class Robot:
     MAX_SPEED = 100
+    MIN_TURN = math.radians(5)
 
     CAMERA_HOME = CameraAngle(90, 25)
     CAMERA_YAW_MIN = 0
@@ -44,21 +52,21 @@ class Robot:
     ID_SERVO_YAW = 1
     ID_SERVO_PITCH = 2
 
-    REG_MOTOR       = 0x01
-    REG_SERVO       = 0x02
+    REG_MOTOR = 0x01
+    REG_SERVO = 0x02
     REG_LED_INT_ALL = 0x03
     REG_LED_INT_ONE = 0x04
-    REG_IR_SWITCH   = 0x05
-    REG_BUZZER      = 0x06
-    REG_UNKNOWN_07  = 0x07 # "Ctrl_Ulatist_Switch" or "Ctrl_getDis_Switch" Ultrasonic distance sensor?
+    REG_IR_SWITCH = 0x05
+    REG_BUZZER = 0x06
+    REG_UNKNOWN_07 = 0x07  # "Ctrl_Ulatist_Switch" or "Ctrl_getDis_Switch" Ultrasonic distance sensor?
     REG_LED_RGB_ALL = 0x08
     REG_LED_RGB_ONE = 0x09
-    REG_IR_TRACK    = 0x0a
+    REG_IR_TRACK = 0x0A
     # What about 0x0b?
-    REG_UNKNOWN_0C  = 0x0c # Reading IR sensor data?
+    REG_UNKNOWN_0C = 0x0C  # Reading IR sensor data?
     # What about 0x0d-0x19?
-    REG_UNKNOWN_1A  = 0x1a # Reading "Ulatist" "diss_L" (diss HIGH byte) Ultrasonic distance (millimeters)?
-    REG_UNKNOWN_1B  = 0x1b # Reading "Ulatist" "diss_H" (diss LOW byte)  Ultrasonic distance (millimeters)?
+    REG_UNKNOWN_1A = 0x1A  # Reading "Ulatist" "diss_L" (diss HIGH byte) Ultrasonic distance (millimeters)?
+    REG_UNKNOWN_1B = 0x1B  # Reading "Ulatist" "diss_H" (diss LOW byte)  Ultrasonic distance (millimeters)?
     # Any more registers?
     # Potentiometers on the front control "distance from track sensors"
     # Can track data be measured or is it just a binary register?
@@ -66,17 +74,15 @@ class Robot:
     # Is ultrasonic a one-time pulse or a continuous measurement?
     # Bot can be remote controlled, is 0x05 and 0x0c for enabling and reading this?
 
-
     def __init__(self):
         self.__bot = Raspbot()
         self.reset()
 
-    def reset(self):
+    def reset(self) -> None:
         """
         Zeros the motors, homes the camera servos, disables lights and switches,
         and zeros the estimated and target states.
         """
-
         self.__bot.Ctrl_Muto(self.ID_MOTOR_FL, 0)
         self.__bot.Ctrl_Muto(self.ID_MOTOR_RL, 0)
         self.__bot.Ctrl_Muto(self.ID_MOTOR_FR, 0)
@@ -89,32 +95,34 @@ class Robot:
         self.__bot.Ctrl_Ulatist_Switch(False)
 
         self.__motor_state = MotorState()
-        self.__pos = Point(0, 0)
-        self.__target_pos = Point(0, 0)
-        self.__angle = 0
-        self.__target_angle = 0
+        self.__pos = Point(0.0, 0.0)
+        self.__target_pos = Point(0.0, 0.0)
+        self.__angle = 0.0
+        self.__target_angle = 0.0
         self.__camera_angle = copy(self.CAMERA_HOME)
         self.__camera_target_angle = copy(self.CAMERA_HOME)
         self.__update_time = time.time()
 
-    def turn_towards(self, angle):
+    def turn_towards(self, angle: float) -> None:
         """
         Sets the bot's target angle within arbitrary bot-space.
         Units are degrees from the initial angle.
 
         Args:
-            angle (float): New target angle. +A = Counter-clockwise
+            angle (float): New target angle (degrees) relative to init.
+                           (+A = Counter-clockwise)
         """
-        self.__target_angle = angle
+        self.__target_angle = math.radians(angle)
 
-    def get_angle(self):
+    def get_angle(self) -> float:
         """
         Returns:
-            float: Estimated angle relative to init. (+A = Counter-clockwise)
+            float: Estimated angle (degrees) relative to init.
+                   (+A = Counter-clockwise)
         """
-        return self.__angle
+        return math.degrees(self.__angle)
 
-    def move_towards(self, x, y):
+    def move_towards(self, x: float, y: float) -> None:
         """
         Sets the bot's target position within arbitrary bot-space.
         Units are "distance traveled in 1 direction in a second."
@@ -126,7 +134,7 @@ class Robot:
         self.__target_pos.x = x
         self.__target_pos.y = y
 
-    def move_relative(self, dx, dy, dangle):
+    def move_relative(self, dx: float, dy: float, dangle: float) -> None:
         """
         Sets the bot's target position relative to its current position within
         arbitrary bot-space. Calling this twice will *not* add the arguments.
@@ -134,24 +142,34 @@ class Robot:
         Args:
             dx (float): Lateral distance component. +X = Right
             dy (float): Longitudinal distance component. +Y = Forward
-            dangle (float): New angle relative to current angle. +A = Counter-clockwise
+            dangle (float): New angle relative to current angle (degrees).
+                            (+A = Counter-clockwise)
         """
+        move_rads = self.__angle + math.atan2(dy, dx)
+        move_dist = math.sqrt(dx * dx + dy * dy)
 
-    def get_position(self):
+        self.__target_pos.x = self.__pos.x + move_dist * math.cos(move_rads)
+        self.__target_pos.y = self.__pos.y + move_dist * math.sin(move_rads)
+        self.__target_angle = self.__angle + dangle
+
+    def get_position(self) -> Point:
         """
         Returns:
             Point: Gets the bot's current estimated position.
         """
         return copy(self.__pos)
 
-    def home_camera_angle(self):
+    def home_camera_angle(self) -> None:
         """
         Sets the target angle of the camera servos to point straight forward.
+        Note that these cheap servos have *a lot* of backlash.
+        Angles are very rough estimates at best and accuracy is both poor and
+        variable.
         """
         self.__camera_target_angle.yaw = 90
         self.__camera_target_angle.pitch = 25
 
-    def set_camera_angle(self, yaw, pitch):
+    def set_camera_angle(self, yaw: int, pitch: int) -> None:
         """
         Sets the target angle of the camera servos.
         Note that these cheap servos have *a lot* of backlash.
@@ -159,14 +177,14 @@ class Robot:
         variable.
 
         Args:
-            yaw (float): Rotation around the vertical axis. 0 faces right, 180
+            yaw (int): Rotation around the vertical axis. 0 faces right, 180
             left.
-            pitch (float): Rotation around the lateral axis. 0 faces down, 90
+            pitch (int): Rotation around the lateral axis. 0 faces down, 90
         """
         self.__camera_target_angle.yaw = yaw
         self.__camera_target_angle.pitch = pitch
 
-    def get_camera_angle(self):
+    def get_camera_angle(self) -> CameraAngle:
         """
         Returns:
             CameraAngle: Gets current camera angles.
@@ -176,23 +194,60 @@ class Robot:
         """
         return copy(self.__camera_angle)
 
-    def get_update_time(self):
+    def get_update_time(self) -> float:
         """
         Returns:
-            float: System time (seconds) of last update call. Target adjustments
-            and state interpolation of the next update are made relative to this
-            time.
+            float: System time (seconds) of last update call. Target
+            adjustments and state interpolation of the next update are made
+            relative to this time.
         """
         return self.__update_time
 
-    def update(self):
-        self.__lerp_state()
+    def update(self, period: float) -> None:
+        """
+        Estimates current world state based on previous movement parameters,
+        updates motor parameters based on new targets
+
+        Args:
+            period (float): Expected time (seconds) before next update() call.
+        """
+        last_time = self.__update_time
+        next_time = time.time()
+        time_diff = next_time - last_time
+
+        self.__lerp_state(time_diff)
+
+        state = self.__motor_state
+        state.vx = self.__target_pos.x - self.__pos.x
+        state.vy = self.__target_pos.y - self.__pos.y
+        state.va = self.__target_angle - self.__angle
+
         # TODO determine motion vector (speed/angle/angular)
         # TODO translate to wheel and camera movements (based on update freq)
         # TODO update motor/servo parameters
 
-    def __lerp_state(self):
-        last_time = self.__update_time
-        next_time = time.time()
-        time_diff = next_time - last_time
-        # TODO the rest of the math :p
+    def __lerp_state(self, time_diff: float):
+        state = self.__motor_state
+        rads = time_diff * state.va
+        if -Robot.MIN_TURN < rads < Robot.MIN_TURN:
+            self.__pos.x += time_diff * state.vx
+            self.__pos.y += time_diff * state.vy
+            self.__angle += rads
+            return
+
+        vel_angle = math.atan2(state.vy, state.vx)
+        dist = time_diff * math.sqrt(state.vx * state.vx + state.vy * state.vy)
+
+        radius = abs(dist / rads)
+        normal_scale = time_diff * radius / dist
+
+        if rads > 0:  # left turn
+            relx = normal_scale * -state.vy + radius * math.sin(vel_angle + rads)
+            rely = normal_scale * state.vx - radius * math.cos(vel_angle + rads)
+        else:  # right turn
+            relx = normal_scale * state.vy - radius * math.sin(vel_angle + rads)
+            rely = normal_scale * -state.vx - radius * math.sin(vel_angle + rads)
+
+        self.__pos.x += relx
+        self.__pos.y += rely
+        self.__angle += rads
